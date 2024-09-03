d06bd05247378970c2505fa1c38eb143353dbd7c
Hudson Ruger <rugerhudson@icloud.com>
2024-09-03 13:55:27 -0400

    Initial Commit

---
+++ Solitaire/SolitaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolitaireApp.swift
+//  Solitaire
+//
+//  Created by Hudson Ruger on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SolitaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
